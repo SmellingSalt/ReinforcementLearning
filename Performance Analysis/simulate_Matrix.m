12b991580e0d7e127baef7f31bafb34ce3ed6896
Balbheem <helpingtutors@gmail.com>
2019-08-28 12:02:05 +0530

    Comments

--- Performance Analysis/simulate_Matrix.m
+++ Performance Analysis/simulate_Matrix.m
@@ -1,10 +1,10 @@
 
-n=input('enter # of trials');
-arms=input('enter # of arms, select within [2,5,10]');
-arm_reward=[10,50,20,5,30,40,70,15,25,80];
-prob_arm_win=[0.4,0.6,0.5,0.9,0.3,0.7,0.8,0.5,0.6,0.3];
-mat = zeros(n,arms);
-for i=1:n
+Trials=1000;        %% Trials=input('enter # of trials');  How many times you want to play?
+arms = 10;          %% arms=input('enter # of arms, select within [2,5,10,50]'); How many arms are available to draw? 
+arm_reward=[10,50,20,5,30,40,70,15,25,80];      %% Reward fixed for each arm
+prob_arm_win=[0.4,0.6,0.5,0.9,0.3,0.7,0.8,0.5,0.6,0.3];         %% Probability of winning for a selected arm
+mat = zeros(Trials,arms);       %% Initialize the final matrix with #rows as Trails and #columns as arms available
+for i=1:Trials
     space=zeros(1,arms);
     arm_selected=randi(arms,1,1);
     if(round(rand*10,0)>5)
