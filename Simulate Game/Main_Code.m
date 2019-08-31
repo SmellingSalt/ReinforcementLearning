b3dcba60aa6426455db4712d5727e57aae3e9121
CHANDAN N <191081002@iitdh.ac.in>
2019-08-31 11:17:22 -0700

    plot update

--- Simulate Game/Main_Code.m
+++ Simulate Game/Main_Code.m
@@ -1,7 +1,7 @@
 %% PLAYING THE GAME
 
 %% SETTING VARIABLES
-T=10000;
+T=100;
 repeat_game=10;
 arms=10;
 game_mat = zeros(repeat_game,T,arms);
@@ -23,4 +23,29 @@ for i=1:repeat_game
     end
 end
 %% VIEW A SAMPLE GAME
-view_a_game=squeeze(game_mat(1,:,:));
+%view_a_game=squeeze(game_mat(1,:,:));
+
+%% PERFORMANCE ANALYSIS
+% regret per turn (1000 turns)
+best_reward=bandit.mu_star*T;
+for 
+%assign to regret and turn in main program
+
+rewards=game_mat(game_mat~=-1);
+regret= 0.001*randi(100,1,1000);
+turn = 1:1:1000;
+plot(turn,regret);
+subplot(1,1,1);
+title('Regret per turn');
+xlabel('number of turns');
+ylabel('regret in that turn');
+
+% percent of optimal arm played
+
+optimal_arm_pulled_count = exp(randi(10,1,1000));
+plot(turn,optimal_arm_pulled_count);
+subplot(1,2,2);
+title('%of optimal arm played');
+xlabel('Total number of plays');
+ylabel('Number of times optimal arm is pulled');
+%
